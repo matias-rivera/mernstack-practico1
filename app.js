e9e031f61c7e85b869f7707725c0ac4b52782c41
matias-rivera <juanmatiasrivera@outlook.com>
2021-11-17 18:25:49 -0300

    Add function getCountriesBySubregion

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function getCountryTranslatedName(isoCode, language){
  * @returns {Array}
  */
 function getCountriesBySubregion(subregion){
-    //Complete with your code
+    return countries.filter(country => country.subregion === subregion);
 }
 
 function main() {
